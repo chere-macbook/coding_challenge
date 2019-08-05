45b97b8d6b83b3997cd24b45f8b5952cb9c85715
Chere Mac <cheremac@Cheres-MacBook-Pro.local>
2019-08-05 13:06:42 +0530

    Refactor as per single responsibility

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -7,19 +7,25 @@ class Reading < ApplicationRecord
   validates :temperature, :humidity, :battery_charge,
             numericality: true, allow_nil: true
 
+  scope :tracking_number_details, -> (tracking_number) { where(tracking_number: tracking_number) }
+
   def process_thermostat_readings
     ReadingWorker.perform_async(self.to_json)
   end
 
-  def self.thermostat_data(tracking_number)
-    # If we are assuming small amount of queue we can use below logic else we need to
-    # change the logic of searching queue to some caching technique or redis implementation
-    job_queue = Sidekiq::Queue.new
-    data = job_queue.select { |queue| queue.klass == 'ReadingWorker' }
-    .collect(&:args).flatten.map { |reading| reading.class == String ? JSON.parse(reading) : reading }
-    .find { |parsed_data| parsed_data['tracking_number'] == tracking_number.to_i }
-    data = Reading.where(tracking_number: tracking_number).first unless data.present?
-    data
+  class << self
+    def thermostat_data(tracking_number)
+      data = search_in_sidekiq(tracking_number)
+      data = tracking_number_details(tracking_number).first unless data.present?
+      data
+    end
+
+    def search_in_sidekiq(tracking_number)
+      job_queue = Sidekiq::Queue.new
+      job_queue.select { |queue| queue.klass == 'ReadingWorker' }
+      .collect(&:args).flatten.map { |reading| reading.class == String ? JSON.parse(reading) : reading }
+      .find { |parsed_data| parsed_data['tracking_number'] == tracking_number.to_i }
+    end
   end
 
   private
